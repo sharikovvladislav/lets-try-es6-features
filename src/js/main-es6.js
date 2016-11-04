86f9dbf823aca39faffd557220cf5d11c87064e8
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-11-04 21:00:59 +0300

    Try classes

--- src/js/main-es6.js
+++ src/js/main-es6.js
@@ -3,6 +3,30 @@ for (let i of array) {
   console.log(i);
 }
 
+class Animal {
+  sound () {
+    console.log('abstract');
+  }
+}
+
+class Cat extends Animal {
+  sound () {
+    console.log('Meow');
+  }
+}
+
+class Dog extends Animal {
+  sound () {
+    console.log('Bark');
+  }
+}
+
+var cat = new Cat();
+cat.sound();
+
+var dog = new Dog();
+dog.sound();
+
 document.addEventListener("DOMContentLoaded", function () {
   var buttons = document.getElementsByTagName('button');
   for (let i = 0; i < buttons.length; i++) {
